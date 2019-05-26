98c6fa77e2dfb4f5da153c8bec555822c2c35074
Moonkaman <nickbatmanglidj@gmail.com>
2019-05-26 11:09:12 -0600

    New table migration

---
+++ db/migrations/20190526110550_user_houses-table.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('user_houses', t => {
+    t.increments();
+    t.integer('user_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('users')
+    .onDelete('CASCADE');
+
+    t.integer('house_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('house')
+    .onDelete('CASCADE');
+
+    t.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  
+};
